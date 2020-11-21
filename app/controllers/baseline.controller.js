f675e66af2056d50c378403a2b563a1a47d84c40
Sergio Neves Barros <s.nevesbarros@googlemail.com>
2020-11-21 17:32:53 +0000

    added additional functionality to baseline api and to validation. Once done i'll look to add it to the ui to determine which image to compare against.

--- app/controllers/baseline.controller.js
+++ app/controllers/baseline.controller.js
@@ -2,6 +2,16 @@ const { validationResult } = require('express-validator');
 const Baseline = require('../models/baseline.js');
 const Screenshot = require('../models/screenshot.js');
 
+const hasValidPlatformDetails = ({ platform }) => {
+  // a platform name must be provided (and a device name or browser name)
+  if (platform.platformName !== undefined
+    && (platform.deviceName !== undefined || platform.browserName !== undefined)) {
+    return true;
+  }
+  // Desktop, then we use resolution and browserName
+  return false;
+};
+
 // Create and save a new test execution
 exports.create = (req, res) => {
   // check the request is valid
@@ -11,28 +21,67 @@ exports.create = (req, res) => {
   }
   const promises = [
     Screenshot.findById(req.body.screenshotId).exec(),
-    Baseline.findOne({ view: req.body.view, deviceName: req.body.deviceName }).exec(),
+    Baseline.find({ view: req.body.view }).exec(),
   ];
   return Promise.all(promises).then((results) => {
     const screenshot = results[0];
-    const baselineFound = results[1];
+    const baselinesFound = results[1];
     if (!screenshot) {
       return res.status(404).send({
         message: `No screenshot found with id ${req.body.screenshotId}`,
       });
     }
-    if (baselineFound !== null) {
-      return res.status(409).send({
-        message: `Baseline for view [${baselineFound.view}] and device [${baselineFound.deviceName}] already exists`,
+    if (screenshot.view !== req.body.view) {
+      return res.status(400).send({
+        message: `The screenshot with id ${req.body.screenshotId} is not for the same view.`,
       });
     }
+    if (screenshot.platform === undefined) {
+      return res.status(400).send({
+        message: `The screenshot with id ${req.body.screenshotId} does not have platform details set. Platform details are required when setting a baseline image.`,
+      });
+    }
+    if (!hasValidPlatformDetails(screenshot)) {
+      return res.status(400).send({
+        message: `The screenshot with id ${req.body.screenshotId} does not have valid platform details set. Please ensure that platform is set for the screenshot (with device name or browserName set)`,
+      });
+    }
+
+    if (screenshot.platform.deviceName) {
+      const matchingBaselines = baselinesFound.filter((baseline) => baseline
+        .platform.platformName === screenshot.platform.platformName
+        && baseline.platform.deviceName === screenshot.platform.deviceName);
+      if (matchingBaselines.length > 0) {
+        return res.status(409).send({
+          message: `Baseline for view [${screenshot.view}], platform [${screenshot.platform.platformName}] and device [${screenshot.platform.deviceName}] already exists`,
+        });
+      }
+    }
+
+    if (!screenshot.platform.deviceName) {
+      const matchingBaselines = baselinesFound.filter((baseline) => baseline
+        .platform.platformName === screenshot.platform.platformName
+        && baseline.platform.browserName === screenshot.platform.browserName
+        && baseline.screenHeight === screenshot.height
+        && baseline.screenWidth === screenshot.width);
+      if (matchingBaselines.length > 0) {
+        return res.status(409).send({
+          message: `Baseline for view [${screenshot.view}], platform [${screenshot.platform.platformName}] and browser [${screenshot.platform.browserName}] with resolution [${screenshot.width} x ${screenshot.height}] already exists`,
+        });
+      }
+    }
+
     // create the baseline.
     const baseline = new Baseline({
       screenshot,
       view: req.body.view,
-      deviceName: req.body.deviceName,
-      height: screenshot.height,
-      width: screenshot.width,
+      platform: {
+        platformName: screenshot.platform.platformName,
+        deviceName: screenshot.platform.deviceName,
+        browserName: screenshot.platform.browserName,
+      },
+      screenHeight: screenshot.height,
+      screenWidth: screenshot.width,
     });
     return baseline.save();
   }).then((data) => {
@@ -45,7 +94,19 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-  Baseline.find()
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  const baseLineQuery = {
+    view: req.query.view,
+    'platform.platformName': req.query.platformName,
+  };
+  if (req.query.deviceName) baseLineQuery.platform.browserName = req.query.deviceName;
+  if (req.query.browserName) baseLineQuery.platform.browserName = req.query.browserName;
+  if (req.query.screenHeight) baseLineQuery.screenHeight = req.query.screenHeight;
+  if (req.query.screenWidth) baseLineQuery.screenWidth = req.query.screenWidth;
+  return Baseline.find(baseLineQuery)
     .then((baselines) => {
       res.send(baselines);
     }).catch((err) => {
