5d26be1d1bc8fe143846be9f4b566bbaf65af4e2
Sergio Neves Barros <s.nevesbarros@googlemail.com>
2026-03-22 08:10:12 +0000

    upgrading api description from swagger to open api

--- swagger/routes/routes.js
+++ swagger/routes/routes.js
@@ -2,11 +2,24 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 module.exports = (app) => {
-  if (process.env.REACT_APP_SWAGGER_ANGLES_API_URL) {
-    swaggerDocument.host = process.env.REACT_APP_SWAGGER_ANGLES_API_URL;
-  }
+  let scheme = 'http';
   if (process.env.REACT_APP_SWAGGER_SCHEMES) {
-    swaggerDocument.schemes = process.env.REACT_APP_SWAGGER_SCHEMES.split(',');
+    scheme = process.env.REACT_APP_SWAGGER_SCHEMES.split(',')[0].trim();
   }
+
+  let host = '127.0.0.1:3000';
+  if (process.env.REACT_APP_SWAGGER_ANGLES_API_URL) {
+    host = process.env.REACT_APP_SWAGGER_ANGLES_API_URL;
+  }
+
+  const serverUrl = host.startsWith('http') ? host : `${scheme}://${host}`;
+
+  swaggerDocument.servers = [
+    {
+      url: `${serverUrl}/rest/api/v1.0`,
+      description: 'Angles API Server'
+    }
+  ];
+
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
