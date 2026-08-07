92a6cf94bd944763510d120c23e2668d432558c3
Sergio Neves Barros <s.nevesbarros@googlemail.com>
2026-08-07 09:27:45 +0100

    Docker image improvements (#111)
    * docker image and config improvements
    
    * minor tweaks on arguments

--- swagger/routes/routes.js
+++ swagger/routes/routes.js
@@ -3,23 +3,32 @@ const swaggerDocument = require('../swagger.json');
 
 module.exports = (app) => {
   let scheme = 'http';
-  if (process.env.REACT_APP_SWAGGER_SCHEMES) {
-    scheme = process.env.REACT_APP_SWAGGER_SCHEMES.split(',')[0].trim();
+  if (process.env.SWAGGER_SCHEMES) {
+    scheme = process.env.SWAGGER_SCHEMES.split(',')[0].trim();
   }
 
   let host = '127.0.0.1:3000';
-  if (process.env.REACT_APP_SWAGGER_ANGLES_API_URL) {
-    host = process.env.REACT_APP_SWAGGER_ANGLES_API_URL;
+  if (process.env.ANGLES_API_BASE_URL) {
+    if (process.env.ANGLES_API_BASE_PATH) {
+      host = `${process.env.ANGLES_API_BASE_URL}${process.env.ANGLES_API_BASE_PATH}`;
+    } else {
+      host = process.env.ANGLES_API_BASE_URL;
+    }
   }
 
   const serverUrl = host.startsWith('http') ? host : `${scheme}://${host}`;
 
-  swaggerDocument.servers = [
-    {
-      url: `${serverUrl}/rest/api/v1.0`,
-      description: 'Angles API Server'
-    }
-  ];
+  // build the served spec from a shallow copy; require() caches swagger.json and
+  // returns a shared object, so mutating it would leak to any other consumer.
+  const document = {
+    ...swaggerDocument,
+    servers: [
+      {
+        url: `${serverUrl}`,
+        description: 'Angles API Server',
+      },
+    ],
+  };
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
 };
